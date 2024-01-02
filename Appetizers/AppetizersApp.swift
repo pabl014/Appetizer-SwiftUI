7ee4dbd4f4aedd943bae20a05e3230d320b9de79
Pawel Rudnik <pawelrudnik3@gmail.com>
2024-01-02 16:54:17 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Paweł Rudnik on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
