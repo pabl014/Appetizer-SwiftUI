5d7ae682cb2c35a1420bf36ca18190847d59b72f
Pawel Rudnik <pawelrudnik3@gmail.com>
2024-01-04 18:30:08 +0100

    Apply LoadingView to AppetizerListView

--- Appetizers/AppetizerListViewModel.swift
+++ Appetizers/AppetizerListViewModel.swift
@@ -12,35 +12,36 @@ import Observation // https://www.youtube.com/watch?v=EK7SthdWV2w
     
     var appetizers: [Appetizer] = []
     var alertItem: AlertItem?
+    var isLoading = false
     
     func getAppetizers() {
         
+        isLoading = true
+        
         NetworkManager.shared.getAppetizers { result in
             
+            self.isLoading = false
             DispatchQueue.main.async { [self] in
-                
                 switch result {
                     
                     case .success(let appetizers): // let appetizers -> we are giving a variable name for what we get back here
-                        
                         self.appetizers = appetizers // self.appetizers : major object, = appetizers : line above
-                        
+                    
                     case .failure(let error):
-                        
                         switch error {
-                            
-                        case .invalidURL:
-                            alertItem = AlertContext.invalidURL
-                            
-                        case .invalidResponse:
-                            alertItem = AlertContext.invalidResponse
-                            
-                        case .invalidData:
-                            alertItem = AlertContext.invalidData
-                            
-                        case .unableToComplete:
-                            alertItem = AlertContext.unableToComplete
-                    }
+                                
+                            case .invalidURL:
+                                alertItem = AlertContext.invalidURL
+                                
+                            case .invalidResponse:
+                                alertItem = AlertContext.invalidResponse
+                                
+                            case .invalidData:
+                                alertItem = AlertContext.invalidData
+                                
+                            case .unableToComplete:
+                                alertItem = AlertContext.unableToComplete
+                        }
                 }
             }
         }
