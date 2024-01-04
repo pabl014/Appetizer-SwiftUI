e795e40c2b8c5e92214b1ac4a5c4cd21caf5f97e
Pawel Rudnik <pawelrudnik3@gmail.com>
2024-01-05 00:56:37 +0100

    Add AppetizerDetailView design. Add extension to replace cornerRadius().

---
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -0,0 +1,112 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Paweł Rudnik on 04/01/2024.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        VStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 225)
+                .frame(maxWidth: .infinity)
+                .clipped()
+            
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .padding(.top, 8)
+                    
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack(spacing: 40) {
+                    VStack(spacing: 5) {
+                        Text("Calories")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.calories)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Carbs")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.carbs)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                    VStack(spacing: 5) {
+                        Text("Protein")
+                            .bold()
+                            .font(.caption)
+                        Text("\(appetizer.protein)")
+                            .foregroundStyle(.secondary)
+                            .fontWeight(.semibold)
+                            .italic()
+                    }
+                    
+                }
+            }
+            
+            Spacer()
+            
+            Button {
+                // add implementation
+            } label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundStyle(.white)
+                    .background(Color.brandPrimary)
+                    .addBorder(.clear, width: 0, cornerRadius: 12)
+            }
+            .padding(.bottom, 30)
+        }
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground)) // blurred back
+        .addBorder(.white, width: 0, cornerRadius: 12)
+        .shadow(radius: 40)
+        .overlay(Button {
+            //add implementation
+        } label: {
+            ZStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundStyle(.white)
+                    .opacity(0.6)
+                
+                Image(systemName: "xmark")
+                    .imageScale(.small)
+                    .frame(width: 44, height: 44)
+                    .foregroundStyle(.black)
+            }
+        }, alignment: .topTrailing)
+    }
+    
+}
+
+#Preview {
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+        .preferredColorScheme(.light)
+}
+
+
+
---
+++ Appetizers/Utilities/Extensions/View+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  View+Ext.swift
+//  Appetizers
+//
+//  Created by Paweł Rudnik on 04/01/2024.
+//
+
+import SwiftUI
+
+// .cornerRadius() replacement, because it will be deprecated soon
+extension View {
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
+
+// usage: 
+//.addBorder(Color.white, width: 1, cornerRadius: 10)
