0a0cb768f720e772b5fbeeeb778ee2b62c492c11
Pawel Rudnik <pawelrudnik3@gmail.com>
2024-01-05 14:51:40 +0100

    Customize AccountView

--- Appetizers/Screens/AppetizerAccountView/AccountView.swift
+++ Appetizers/Screens/AppetizerAccountView/AccountView.swift
@@ -8,10 +8,49 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("AccountView")
-                .navigationTitle("🙍 Account")
+            
+            Form {
+                Section() {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                    
+                    DatePicker("Birthday", selection: $birthdate,
+                               displayedComponents: .date)
+                    
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                    
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                } header: {
+                    Text("Requests")
+                }
+                .tint(.brandPrimary)
+
+            }
+            .navigationTitle("👤 Account")
         }
     }
 }
@@ -19,3 +58,4 @@ struct AccountView: View {
 #Preview {
     AccountView()
 }
+
