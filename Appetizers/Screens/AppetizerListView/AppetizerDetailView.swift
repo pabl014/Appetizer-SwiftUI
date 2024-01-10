005b15b7dc60c86fd0c156b50b4cef3dc20577ab
Pawel Rudnik <pawelrudnik3@gmail.com>
2024-01-10 21:39:03 +0100

    Remove grams from calories in DetailView

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -35,9 +35,9 @@ struct AppetizerDetailView: View {
                     .padding()
                 
                 HStack(spacing: 40) {
-                    NutritionInfo(title: "Calories", value: appetizer.calories)
-                    NutritionInfo(title: "Carbs", value: appetizer.carbs)
-                    NutritionInfo(title: "Protein", value: appetizer.protein)
+                    NutritionInfo(title: "Calories", value: "\(appetizer.calories)")
+                    NutritionInfo(title: "Carbs", value: "\(appetizer.carbs) g")
+                    NutritionInfo(title: "Protein", value: "\(appetizer.protein) g")
                 }
             }
             
@@ -74,21 +74,17 @@ struct AppetizerDetailView: View {
 struct NutritionInfo: View {
     
     let title: String
-    let value: Int
+    let value: String
     
     var body: some View {
         VStack(spacing: 5) {
             Text(title)
                 .bold()
                 .font(.caption)
-            Text("\(value)")
+            Text(value)
                 .foregroundStyle(.secondary)
                 .fontWeight(.semibold)
                 .italic()
-                + Text(" g")
-                    .foregroundColor(.secondary)
-                    .fontWeight(.semibold)
-                    .italic()
         }
     }
 }
